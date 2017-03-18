725f705c05917780c29326264dfca2572756f2e4
Mihai Sandor <irc.host@yahoo.com>
2017-03-18 17:28:16 +0200

    Update

--- FlappyCLI.sh
+++ FlappyCLI.sh
@@ -2,7 +2,7 @@
 #Created by MihaiSandor on 18-Mar-2017
 
 p=python
-if (( $p ))
+if (( p ))
     f=flappy.py
     $p $f "$1"
 then
@@ -10,14 +10,14 @@ then
 fi
 
 p3=python3
-if (( $p3 ))
+if (( p3 ))
     f=flappylang.py
     $p3 $f "$1"
 then
     sleep 0
 fi
 
-if ! (( $p && $p3 ))
+if ! (( p && p3 ))
 then
     cd bin && ./flappy $1
 fi
