e673f9d30cbaef238e3865c5993860a02e780924
Mihai Sandor <irc.host@yahoo.com>
2017-03-18 17:05:24 +0200

    Update

--- FlappyCLI.sh
+++ FlappyCLI.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
+#Created by MihaiSandor on 18-Mar-2017
 
 p=python
-f=flappy.py
-$p $f $1
+if (( $p ))
+    f=flappy.py
+    $p $f "$1"
+then
+    exit;
+fi
 
-p=python3
-f=flappylang.py
-$p $f $2
+p3=python3
+if (( $p3 ))
+    f=flappylang.py
+    $p3 $f "$1"
+then
+    sleep 0
+fi
+
+if ! (( $p && $p3 ))
+then
+    cd bin && ./flappy $1
+fi
